0890f12690e0f054371770a694e55ec7e8e86c26
ishimwe christian <christianishimwe72@gmail.com>
2022-06-30 12:00:05 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
-
-
-
 /**
  *
- *  * main - Entry point
+ * main - Entry point
  *
- *   *
- *
- *    * Return: 0 after printing the function
- *
- *     */
+ * Return: 0 after printing the function
+ */
 
 int main(void)
 
-{puts("\"Programing is like building a multilingual puzzle");
+{puts("\"Programming is like building a multilingual puzzle");
 
 	    return (0);
 
