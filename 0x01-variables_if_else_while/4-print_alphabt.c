6d51b82d753479fd2b506975dac75c2d008d2900
ishimwe christian <christianishimwe72@gmail.com>
2022-07-05 12:29:23 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+if (a == 'q' || a == 'e')
+}
+continue;
+putchar (a);
+return (0);
+}
