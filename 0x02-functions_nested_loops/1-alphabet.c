e63e6828f52aae8a80d61f372e38b70aeade8531
ishimwe christian <christianishimwe72@gmail.com>
2022-07-07 05:39:11 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * print_alphabet -> prints the lowercase alphabets
+ **/
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
