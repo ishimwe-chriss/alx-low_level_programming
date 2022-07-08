223deddd9ad2fee183fa3e41895c23bdfcdc48ee
ishimwe christian <christianishimwe72@gmail.com>
2022-07-08 06:34:51 -0700

    Print alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,20 +4,18 @@
 /**
  * main - Entry point
  *
- * Return: Always return 0
+ * Return: Always return 0 (Success)
  */
 int main(void)
 {
-     char low, e, q;
-
-     e = 'e';
-     q = 'q';
-
-    for (low = 'a'; low <= 'z'; low++)
-    {
-             if (low ='e' && low = 'q')
-		     putchar(low);
-    }
-                       printf("\n");
-     return (0);
+char low, e, q;
+e = 'e';
+q = 'q';
+for (low = 'a'; low <= 'z'; low++)
+{
+if (low != 'e' && low != 'q')
+putchar(low);
+}
+printf("\n");
+return (0);
 }
