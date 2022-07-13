ec75d57a9a6cc0aa6f477910ea6f142e82c32fc9
ishimwe christian <christianishimwe72@gmail.com>
2022-07-13 14:07:09 -0700

    Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,15 +6,12 @@
  * Return: string
  */
 char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
 
-	char *start = dest;
-
-	while (*src != "\0")
+	for (; src[i] != '\0'; i++)
 	{
-		*dest = *src;
-		dest++;
-		src++;
+		dest[i] = src[i];
 	}
-	*dest = "\0";
-	return (start);
+	return (dest);
 }
