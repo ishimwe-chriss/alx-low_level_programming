5bce216e2ce3982a6ac466f9612d95c81398a00a
ishimwe_chriss <christianishimwe72@gmail.com>
2022-08-17 07:11:17 -0700

    Task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,7 +3,8 @@
 /*
  * File: lists.h
  * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes and definitions for all functions and types written in the 0x11-singly_linked_lists directory.
+ * Desc: Header file containing prototypes and definitions for all functions
+ * and types written in the 0x11-singly_linked_lists directory.
  */
 #include <stdlib.h>
 
