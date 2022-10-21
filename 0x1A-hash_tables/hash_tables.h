6627ac07c92741ca30cd3415429d7279f8d66d9d
ishimwe_chriss <christianishimwe72@gmail.com>
2022-10-21 07:50:28 -0700

    Task 7

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -43,6 +43,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
-void free_hash_list(hash_node_t *head);
+goid free_hash_list(hash_node_t *head);
+shash_table_t *shash_table_create(unsigned long int size)
 
 #endif
